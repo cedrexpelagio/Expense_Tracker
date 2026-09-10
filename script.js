a636faf0ddd470494037764043d487f695ac378a
cedrexpelagio <cedrexpelagio@gmail.com>
2026-09-10 23:14:17 +0800

    Refactor expense addition logic to use event listener on form submission
    - Added addExpense function to expense values in the expense list table
    - Added document event DOMContentLoaded

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
-const addExpense = document.getElementById('add-expense-btn');
-const expenseList = document.getElementById('expense-list');
-const totalAmount = document.getElementById('total-amount');
-const filterCategory = document.getElementById('filter-category');
-const expenseForm = document.getElementById('expense-form');
+document.addEventListener("DOMContentLoaded", () => {
+    const expenseList = document.getElementById('expense-list');
+    const totalAmount = document.getElementById('total-amount');
+    const filterCategory = document.getElementById('filter-category');
+    const expenseForm = document.getElementById('expense-form');
 
-addExpense.addEventListener('click', (event) => {
-    
+    const expenseName = document.getElementById('expense-name');
+    const expenseAmount = document.getElementById('expense-amount');
+    const expenseCategory = document.getElementById('expense-category');
+
     let expenses = [];
 
-    event.preventDefault();
-    const expense = {
-        name: document.getElementById('expense-name').value,
-        amount: document.getElementById('expense-amount').value,
-        category: document.getElementById('expense-category').value
-    };
-    expenses.push(expense);
+    function addExpense(expense) {
+
+        const tr = document.createElement('tr');
+
+        tr.innerHTML = `<td>${expense.name}</td>
+                <td><span class="badge badge-food">${expense.category}</span></td>
+                <td class="amount">${expense.category}</td>
+                <td class="actions">
+                  <button type="button" class="btn btn-update">Update</button>
+                  <button type="button" class="btn btn-delete">Delete</button>
+                </td> `;
+
+        expenseList.appendChild(tr);
+
+    }
+
+    expenseForm.addEventListener('submit', (event) => {
+
+        event.preventDefault();
+
+        const expense = {
+            name: expenseName.value,
+            amount: expenseAmount.value,
+            category: expenseCategory.value
+        };
+
+        expenses.push(expense);
+        addExpense(expense);
+
+        expenseForm.reset();
+    });
 });
